66368a2b26a2767a02d5abeaba22f7e8f7ecac42
tw1997 <t888y@qq.com>
2018-04-07 04:18:00 +0800

    fixed bug

--- train.py
+++ train.py
@@ -44,7 +44,6 @@ W_fc2 = weight_variable([1024, 36])
 b_fc2 = bias_variable([36])
 y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2, name='output_label')
 
-sess = tf.Session()
 cross_entropy = -tf.reduce_sum(y_ * tf.log(y_conv))
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
